e51d1a4b6e8c0d71ac1e96de5a2c298e7091f138
Matthew Camp <matthewcampdev@gmail.com>
2024-01-14 11:07:13 -0500

    Adding extensions for accumulating graphs and implemented spending graphs for this month and last month

--- K9-Koinz/Pages/Index.cshtml.cs
+++ K9-Koinz/Pages/Index.cshtml.cs
@@ -7,19 +7,71 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using K9_Koinz.Data;
 using K9_Koinz.Models;
+using K9_Koinz.Utils;
+using Newtonsoft.Json;
+using System.Runtime.Serialization;
+using Microsoft.CodeAnalysis.Elfie.Extensions;
 
 namespace K9_Koinz.Pages {
-    public class IndexModel : PageModel {
-        private readonly K9_Koinz.Data.KoinzContext _context;
 
-        public IndexModel(K9_Koinz.Data.KoinzContext context) {
+    [DataContract]
+    public class Point {
+        [DataMember(Name = "x")]
+        public double X { get; set; }
+        [DataMember(Name = "y")]
+        public double Y { get; set; }
+
+        public Point(double x, double y) {
+            X = x;
+            Y = y;
+        }
+    }
+    public class IndexModel : PageModel {
+        private readonly KoinzContext _context;
+
+        public IndexModel(KoinzContext context) {
             _context = context;
         }
 
         public IList<Budget> Budget { get; set; } = default!;
 
+        public string ThisMonthSpendingJson;
+        public string LastMonthSpendingJson;
+
         public async Task OnGetAsync() {
-            Budget = await _context.Budgets.ToListAsync();
+            var startOfThisMonth = DateTime.Now.StartOfMonth();
+            var endOfThisMonth = DateTime.Now.EndOfMonth();
+
+            var startOfLastMonth = DateTime.Now.AddMonths(-1).StartOfMonth();
+            var endOfLastMonth = DateTime.Now.AddMonths(-1).EndOfMonth();
+
+            var blockedCategoryIds = _context.Categories.Where(cat => cat.Name == "Transfer" || cat.Name == "Income" || (cat.ParentCategoryId.HasValue && (cat.ParentCategory.Name == "Transfer" || cat.ParentCategory.Name == "Income"))).Select(cat => cat.Id).ToList();
+
+            var thisMonthTransactions = await _context.Transactions
+                .Include(trans => trans.Account)
+                .Where(trans => trans.Date >= startOfThisMonth && trans.Date <= endOfThisMonth)
+                .Where(trans => trans.Account.Type == AccountType.CREDIT_CARD || trans.Account.Type == AccountType.CHECKING || trans.Account.Type == AccountType.SAVINGS)
+                .Where(trans => blockedCategoryIds.Contains(trans.Category.Id) == false)
+                .GroupBy(trans => trans.Date)
+                .Select(group => new Point(group.Key.Day, group.Sum(trans => -1 * trans.Amount)))
+                .ToListAsync();
+
+            var lastMonthTransactions = await _context.Transactions
+                .Include(trans => trans.Account)
+                .Where(trans => trans.Date >= startOfLastMonth && trans.Date <= endOfLastMonth)
+                .Where(trans => trans.Account.Type == AccountType.CREDIT_CARD || trans.Account.Type == AccountType.CHECKING || trans.Account.Type == AccountType.SAVINGS)
+                .Where(trans => blockedCategoryIds.Contains(trans.Category.Id) == false)
+                .GroupBy(trans => trans.Date)
+                .Select(group => new Point(group.Key.Day, group.Sum(trans => -1 * trans.Amount)))
+                .ToListAsync();
+
+            ThisMonthSpendingJson = JsonConvert.SerializeObject(thisMonthTransactions.Accumulate().ToList().FillInGaps(DateTime.Now, false), Formatting.None, new JsonSerializerSettings {
+               StringEscapeHandling = StringEscapeHandling.EscapeNonAscii 
+            });
+
+            LastMonthSpendingJson = JsonConvert.SerializeObject(lastMonthTransactions.Accumulate().ToList().FillInGaps(DateTime.Now.AddMonths(-1), true), Formatting.None, new JsonSerializerSettings {
+                StringEscapeHandling = StringEscapeHandling.EscapeNonAscii
+            });
         }
     }
 }
---
+++ K9-Koinz/Utils/ChartUtils.cs
@@ -0,0 +1,37 @@
+﻿using K9_Koinz.Pages;
+
+namespace K9_Koinz.Utils {
+    public static class ChartUtils {
+
+        public static IEnumerable<Point> Accumulate(this IEnumerable<Point> points) {
+            double sum = 0;
+            foreach (var point in points) {
+                sum += point.Y;
+                yield return new Point(point.X, Math.Floor(sum));
+            }
+        }
+
+        public static List<Point> FillInGaps(this List<Point> points, DateTime date, bool doFullMonth) {
+            var newList = new List<Point>();
+            var max = date.Day;
+            if (doFullMonth) {
+                max = DateTime.DaysInMonth(date.Year, date.Month);
+            }
+            var index = 0;
+            for (var i = 1; i < max + 1; i++) {
+                if (points.Any(p => p.X == i)) {
+                    newList.Add(points.Single(p => p.X == i));
+                } else {
+                    if (i == 1) {
+                        newList.Add(new Point(1, 0));
+                    } else {
+                        newList.Add(new Point(i, newList[index - 1].Y));
+                    }
+                }
+                index += 1;
+            }
+
+            return newList;
+        }
+    }
+}
