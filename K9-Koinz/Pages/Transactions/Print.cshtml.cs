fed3f09299dfa171204f1c62d0413527ea5c12ff
Matthew Camp <matthewcampdev@gmail.com>
2024-01-14 09:27:02 -0500

    Adding print view to transactions

---
+++ K9-Koinz/Pages/Transactions/Print.cshtml.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using K9_Koinz.Data;
+using K9_Koinz.Models;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using K9_Koinz.Utils;
+
+namespace K9_Koinz.Pages.Transactions {
+    public class PrintModel : PageModel {
+        private readonly KoinzContext _context;
+        private readonly ILogger<PrintModel> _logger;
+
+        public PrintModel(KoinzContext context, ILogger<PrintModel> logger) {
+            _context = context;
+            _logger = logger;
+        }
+
+        public SelectList AccountOptions;
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? MinDateFilter { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime? MaxDateFilter { get; set; }
+        public Guid? AccountFilter { get; set; }
+
+        public string MinDateString {
+            get {
+                if (MinDateFilter.HasValue) {
+                    return MinDateFilter.Value.FormatForUrl();
+                } else {
+                    return null;
+                }
+            }
+        }
+        public string MaxDateString {
+            get {
+                if (MaxDateFilter.HasValue) {
+                    return MaxDateFilter.Value.FormatForUrl();
+                } else {
+                    return null;
+                }
+            }
+        }
+
+        public List<Transaction> Transactions { get; set; }
+
+        public async Task OnGetAsync(DateTime? minDate, DateTime? maxDate, Guid? accountId) {
+            AccountOptions = new SelectList(_context.Accounts
+                .Where(acct => acct.Type == AccountType.CHECKING || acct.Type == AccountType.CREDIT_CARD || acct.Type == AccountType.SAVINGS)
+                .OrderBy(acct => acct.Name), nameof(Account.Id), nameof(Account.Name));
+
+            if (minDate == null) {
+                minDate = DateTime.Now.StartOfMonth();
+            }
+            if (maxDate == null) {
+                maxDate = DateTime.Now.EndOfMonth();
+            }
+
+            MinDateFilter = minDate;
+            MaxDateFilter = maxDate;
+            AccountFilter = accountId;
+
+            var transactions = _context.Transactions
+                .Include(trans => trans.Category)
+                .Include(trans => trans.Account)
+                .Include(trans => trans.Merchant)
+                .Where(trans => trans.Date >= MinDateFilter.Value && trans.Date <= MaxDateFilter);
+
+            if (accountId != null) {
+                transactions = transactions.Where(trans => trans.AccountId == accountId);
+            }
+
+            Transactions = await transactions.OrderBy(trans => trans.Date).ToListAsync();
+        }
+    }
+}
