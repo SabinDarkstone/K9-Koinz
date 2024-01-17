261371b5d95da63404e38c754eb5b3e2f7b69ee4
Matthew Camp <matthewcampdev@gmail.com>
2024-01-17 11:01:05 -0500

    Fixing pagination text on transaction index

--- K9-Koinz/PaginatedList.cs
+++ K9-Koinz/PaginatedList.cs
@@ -4,10 +4,16 @@ namespace K9_Koinz {
     public class PaginatedList<T> : List<T> {
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
+        public int TotalCount { get; private set; }
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize) {
             PageIndex = pageIndex;
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            TotalCount = count;
+
+            if (count == 0) {
+                TotalPages = 1;
+            }
 
             this.AddRange(items);
         }
