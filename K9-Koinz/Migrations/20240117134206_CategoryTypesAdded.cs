af129679c2efb02012d4167db10b8c6cae43ee47
Matthew Camp <matthewcampdev@gmail.com>
2024-01-17 09:08:20 -0500

    Adding category type migration

---
+++ K9-Koinz/Migrations/20240117134206_CategoryTypesAdded.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace K9_Koinz.Migrations
+{
+    /// <inheritdoc />
+    public partial class CategoryTypesAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryType",
+                table: "Category",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CategoryType",
+                table: "Category");
+        }
+    }
+}
