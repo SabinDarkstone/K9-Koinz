c53dddb9b11fe8fb560405e51691a7e78930a099
Matthew Camp <matthewcampdev@gmail.com>
2024-02-04 15:29:27 -0500

    More models reorganization

--- K9-Koinz/Data/KoinzContext.cs
+++ K9-Koinz/Data/KoinzContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using K9_Koinz.Models;
+using K9_Koinz.Models.Meta;
 
 namespace K9_Koinz.Data {
     public class KoinzContext : DbContext {
--- K9-Koinz/Utils/UniqueAttribute.cs
+++ K9-Koinz/Utils/UniqueAttribute.cs
@@ -1,5 +1,5 @@
 ﻿using K9_Koinz.Data;
-using K9_Koinz.Models;
+using K9_Koinz.Models.Meta;
 using System.ComponentModel.DataAnnotations;
 
 namespace K9_Koinz.Utils {
