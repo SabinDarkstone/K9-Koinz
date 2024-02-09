414ade9bffa89d017df2a6c46b2557b39635d12b
Matthew Camp <matthewcampdev@gmail.com>
2024-02-09 09:04:00 -0500

    Adding ability to create transfers

---
+++ K9-Koinz/Models/Transfer.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace K9_Koinz.Models {
+    public class Transfer {
+        public Guid Id { get; set; }
+        [Required]
+        [DisplayName("From Account")]
+        public Guid FromAccountId { get; set; }
+        public Account FromAccount { get; set; }
+        public string FromAccountName { get; set; }
+        [Required]
+        [DisplayName("To Account")]
+        public Guid ToAccountId { get; set; }
+        public Account ToAccount { get; set; }
+        public string ToAccountName { get; set; }
+        [DataType(DataType.Date, ErrorMessage = "Date only")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Date { get; set; }
+        [Required]
+        [DisplayName("Merchant")]
+        public Guid MerchantId { get; set; }
+        public Merchant Merchant { get; set; }
+        public string MerchantName { get; set; }
+        [Required]
+        [DisplayName("Category")]
+        public Guid CategoryId { get; set; }
+        public Category Category { get; set; }
+        public string CategoryName { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public double Amount { get; set; }
+        public string Notes { get; set; }
+        [DisplayName("Tag")]
+        public Guid? TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
---
+++ K9-Koinz/Pages/Transfers/Create.cshtml.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using K9_Koinz.Data;
+using K9_Koinz.Models;
+using Microsoft.EntityFrameworkCore;
+using Humanizer;
+using K9_Koinz.Utils;
+
+namespace K9_Koinz.Pages.Transfers {
+    public class CreateModel : PageModel {
+        private readonly KoinzContext _context;
+        private readonly ILogger<CreateModel> _logger;
+
+        public CreateModel(KoinzContext context, ILogger<CreateModel> logger) {
+            _context = context;
+            _logger = logger;
+        }
+
+        public List<SelectListItem> AccountOptions;
+        public SelectList TagOptions;
+
+        public void OnGet() {
+            AccountOptions = AccountUtils.GetAccountList(_context, true);
+            TagOptions = TagUtils.GetTagList(_context);
+        }
+
+        [BindProperty]
+        public Transfer Transfer { get; set; } = default!;
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (!ModelState.IsValid) {
+                return Page();
+            }
+
+            Transfer.Date = Transfer.Date.AtMidnight().Add(new TimeSpan(DateTime.Now.Hour, DateTime.Now.Minute, DateTime.Now.Second));
+
+            var category = await _context.Categories.FindAsync(Transfer.CategoryId);
+            var merchant = await _context.Merchants.FindAsync(Transfer.MerchantId);
+            var fromAccount = await _context.Accounts.FindAsync(Transfer.FromAccountId);
+            var toAccount = await _context.Accounts.FindAsync(Transfer.ToAccountId);
+
+            if (Transfer.TagId == Guid.Empty) {
+                Transfer.TagId = null;
+            }
+
+            var fromTransaction = new Transaction {
+                AccountId = Transfer.FromAccountId,
+                AccountName = fromAccount.Name,
+                CategoryId = Transfer.CategoryId,
+                CategoryName = category.Name,
+                MerchantId = Transfer.MerchantId,
+                MerchantName = merchant.Name,
+                Amount = -1 * Transfer.Amount,
+                Notes = Transfer.Notes,
+                TagId = Transfer.TagId,
+                Date = Transfer.Date
+            };
+            var toTransaction = new Transaction {
+                AccountId = Transfer.ToAccountId,
+                AccountName = toAccount.Name,
+                CategoryId = Transfer.CategoryId,
+                CategoryName = category.Name,
+                MerchantId = Transfer.MerchantId,
+                MerchantName = merchant.Name,
+                Amount = Transfer.Amount,
+                Notes = Transfer.Notes,
+                TagId = Transfer.TagId,
+                Date = Transfer.Date
+            };
+
+            _context.Transactions.Add(fromTransaction);
+            _context.Transactions.Add(toTransaction);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Transactions/Index");
+        }
+
+        public IActionResult OnGetMerchantAutoComplete(string text) {
+            text = text.Trim();
+            var merchants = _context.Merchants
+                .AsNoTracking()
+                .AsEnumerable()
+                .Where(merch => merch.Name.Contains(text, StringComparison.CurrentCultureIgnoreCase))
+                .Select(merch => new {
+                    label = merch.Name,
+                    val = merch.Id
+                }).ToList();
+            return new JsonResult(merchants);
+        }
+
+        public IActionResult OnGetCategoryAutoComplete(string text) {
+            text = text.Trim();
+            var categories = _context.Categories
+                .Include(cat => cat.ParentCategory)
+                .AsNoTracking()
+                .AsEnumerable()
+                .Where(cat => cat.FullyQualifiedName.Contains(text, StringComparison.CurrentCultureIgnoreCase))
+                .Select(cat => new {
+                    label = cat.ParentCategoryId != null ? cat.ParentCategory.Name + ": " + cat.Name : cat.Name,
+                    val = cat.Id
+                }).ToList();
+            return new JsonResult(categories);
+        }
+    }
+}
