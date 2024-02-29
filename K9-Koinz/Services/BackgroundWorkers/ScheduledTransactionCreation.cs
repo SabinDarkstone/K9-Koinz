6d841d354751f1e4c65bf01784ea176eb7a23474
Matthew Camp <matthewcampdev@gmail.com>
2024-02-29 09:27:45 -0500

    Making weekly bill calculations work better

--- K9-Koinz/Services/BackgroundWorkers/ScheduledTransactionCreation.cs
+++ K9-Koinz/Services/BackgroundWorkers/ScheduledTransactionCreation.cs
@@ -14,9 +14,19 @@ namespace K9_Koinz.Services.BackgroundWorkers {
         protected override void DoWork(object state) {
             base.DoWork(state);
             _logger.LogInformation("Checking for any transactions that need to be created for bills today: " + DateTime.Today.ToShortDateString());
-            var transactionsCreated = CreateTransactionsForBills(DateTime.Now.AddMinutes(1));
+            var transactionsCreated = new List<Transaction>();
+            var nextMinute = DateTime.Now.AddMinutes(1);
+
+            _logger.LogInformation("Checking for weekly bills...");
+            transactionsCreated.AddRange(CreateTransactionsForBills(nextMinute, Frequency.WEEKLY));
+
+            _logger.LogInformation("Checking for monthly bills...");
+            transactionsCreated.AddRange(CreateTransactionsForBills(nextMinute, Frequency.MONTHLY));
+
+            _logger.LogInformation("Checking for yearly bills...");
+            transactionsCreated.AddRange(CreateTransactionsForBills(nextMinute, Frequency.YEARLY));
             
-            if (transactionsCreated.Any()) {
+            if (transactionsCreated.Count > 0) {
                 transactionsCreated.ForEach(trans => {
                     _logger.LogInformation("Created transaction: " + trans.Id.ToString() + " : " + trans.AccountName
                         + " : " + trans.MerchantName + " : " + trans.CategoryName
@@ -27,45 +37,67 @@ namespace K9_Koinz.Services.BackgroundWorkers {
             }
         }
 
-        private List<Transaction> CreateTransactionsForBills(DateTime date) {
-            var bills = GetBillsFromMonthStartToNow(date);
+        private List<Transaction> CreateTransactionsForBills(DateTime date, Frequency frequency) {
+            DateTime startDate, endDate;
 
-            List<Transaction> transactionsToCreate = new List<Transaction>();
-            foreach (var bill in bills) {
-                var newTransaction = new Transaction {
-                    AccountId = bill.AccountId,
-                    AccountName = bill.AccountName,
-                    BillId = bill.Id,
-                    MerchantId = bill.MerchantId,
-                    MerchantName = bill.MerchantName,
-                    CategoryId = bill.CategoryId.Value,
-                    CategoryName = bill.CategoryName,
-                    Amount = bill.BillAmount * -1,
-                    Date = bill.NextDueDate.Value
-                };
-                transactionsToCreate.Add(newTransaction);
+            switch (frequency) {
+                case Frequency.WEEKLY:
+                    startDate = date.StartOfWeek();
+                    break;
+                case Frequency.MONTHLY:
+                    startDate = date.StartOfMonth();
+                    break;
+                case Frequency.YEARLY:
+                    startDate = date.StartOfYear();
+                    break;
+                default:
+                    return null;
             }
 
-            if (transactionsToCreate.Any()) {
-                _context.Transactions.AddRange(transactionsToCreate);
-                _context.SaveChanges();
+            endDate = date;
 
+            var bills = getBillsForTimePeriod(startDate, endDate);
+            var transactionsCreated = new List<Transaction>();
+
+            while (bills.Count > 0) {
+                var transactionsToCreate = new List<Transaction>();
                 foreach (var bill in bills) {
-                    bill.LastDueDate = bill.NextDueDate;
+                    var newTransaction = new Transaction {
+                        AccountId = bill.AccountId,
+                        AccountName = bill.AccountName,
+                        BillId = bill.Id,
+                        MerchantId = bill.MerchantId,
+                        MerchantName = bill.MerchantName,
+                        CategoryId = bill.CategoryId.Value,
+                        CategoryName = bill.CategoryName,
+                        Amount = bill.BillAmount * -1,
+                        Date = bill.NextDueDate.Value
+                    };
+                    transactionsToCreate.Add(newTransaction);
                 }
 
-                _context.Bills.UpdateRange(bills);
-                _context.SaveChanges();
+                if (transactionsToCreate.Count > 0) {
+                    _context.Transactions.AddRange(transactionsToCreate);
+
+                    foreach (var bill in bills) {
+                        bill.LastDueDate = bill.NextDueDate;
+                    }
+
+                    _context.Bills.UpdateRange(bills);
+                    _context.SaveChanges();
+
+                    transactionsCreated.AddRange(transactionsToCreate);
+                }
+
+                bills = getBillsForTimePeriod(startDate, endDate);
             }
 
-            return transactionsToCreate;
+            return transactionsCreated;
         }
 
-        private List<Bill> GetBillsFromMonthStartToNow(DateTime refDate) {
-            var startDate = refDate.StartOfMonth();
-            return _context.Bills.ToList()
-                .Where(bill => bill.NextDueDate >= startDate)
-                .Where(bill => bill.NextDueDate <= refDate)
+        private List<Bill> getBillsForTimePeriod(DateTime startDate, DateTime endDate) {
+            return _context.Bills.AsEnumerable()
+                .Where(bill => bill.NextDueDate >= startDate && bill.NextDueDate <= endDate)
                 .Where(bill => !bill.IsExpired)
                 .ToList();
         }
