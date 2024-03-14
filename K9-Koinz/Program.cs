ceba5d3de2e8b29fbd5b8c9373fe644102a46acd
Matthew Camp <matthewcampdev@gmail.com>
2024-03-14 14:34:55 -0400

    Removing https stuff

--- K9-Koinz/Program.cs
+++ K9-Koinz/Program.cs
@@ -21,13 +21,6 @@ namespace K9_Koinz {
                 options.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.Warning);
             });
 
-            if (!builder.Environment.IsDevelopment()) {
-                builder.Services.AddHttpsRedirection(options => {
-                    options.RedirectStatusCode = 308;
-                    options.HttpsPort = 443;
-                });
-            }
-
             // Add K9 Koinz Services
             builder.Services.AddMyServices();
 
@@ -38,12 +31,12 @@ namespace K9_Koinz {
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment()) {
-                app.UseHsts();
+                // NO OP
             } else {
                 app.UseMigrationsEndPoint();
             }
 
-            app.UseHttpsRedirection();
+            app.UseHsts();
 
             app.Use(async (context, next) => {
                 if (context.Request.Path.StartsWithSegments("/robots.txt")) {
