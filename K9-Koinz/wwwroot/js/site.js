bf5700f8063cfa0b154610c6da7e435179cae304
Matthew Camp <matthewcampdev@gmail.com>
2024-03-16 12:13:06 -0400

    Refactoring autocomplete code to allow split transactions  to utilize it

--- K9-Koinz/wwwroot/js/site.js
+++ K9-Koinz/wwwroot/js/site.js
@@ -1,72 +1,78 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
-$(function () {
+﻿$(function () {
     $("#txtMerchant").autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                url: "/api/Autocomplete/GetAutocompleteMerchants",
-                data: { text: request.term },
-                type: "GET",
-                success: function (data) {
-                    response($.map(data, function (item) {
-                        return item;
-                    }))
-                },
-                error: function (response) {
-                    alert(JSON.stringify(response));
-                },
-                failure: function (response) {
-                    alert(JSON.stringify(response));
-                }
-            });
-        },
+        source: (request, response) => autocompleteMerchants(request, response),
         select: function (e, i) {
             $("#hfMerchant").val(i.item.val);
-            $.ajax({
-                url: "/api/Autocomplete/GetSuggestedCategory",
-                data: { merchantId: $("#hfMerchant").val() },
-                type: "GET",
-                success: function (data) {
-                    if (data != null) {
-                        $("#hfCategory").val(data.id);
-                        $("#txtCategory").val(data.name);
-                    }
-                },
-                error: function (data) {
-                    console.log(data);
-                },
-                failure: function (data) {
-                    console.log(data);
-                }
-            })
+            getSuggestedCategory($("#hfMerchant").val(), (data) => {
+                $("#hfCategory").val(data.id);
+                $("#txtCategory").val(data.name);
+            });
         },
         minLength: 1
     });
 
     $("#txtCategory").autocomplete({
-        source: function (request, response) {
-            $.ajax({
-                url: "/api/Autocomplete/GetAutocompleteCategories",
-                data: { text: request.term },
-                type: "GET",
-                success: function (data) {
-                    response($.map(data, function (item) {
-                        return item;
-                    }))
-                },
-                error: function (response) {
-                    alert(JSON.stringify(response));
-                },
-                failure: function (response) {
-                    alert(JSON.stringify(response));
-                }
-            });
-        },
+        source: (request, response) => autocompleteCategories(request, response),
         select: function (e, i) {
             $("#hfCategory").val(i.item.val);
         },
         minLength: 1
     });
 });
+
+function getSuggestedCategory(merchantId, callback) {
+    $.ajax({
+        url: "/api/Autocomplete/GetSuggestedCategory",
+        data: { merchantId: merchantId },
+        type: "GET",
+        success: function (data) {
+            if (data != null) {
+                callback(data);
+            }
+        },
+        error: function (data) {
+            console.log(data);
+        },
+        failure: function (data) {
+            console.log(data);
+        }
+    });
+}
+
+function autocompleteMerchants(request, response) {
+    $.ajax({
+        url: "/api/Autocomplete/GetAutocompleteMerchants",
+        data: { text: request.term },
+        type: "GET",
+        success: function (data) {
+            response($.map(data, function (item) {
+                return item;
+            }))
+        },
+        error: function (response) {
+            alert(JSON.stringify(response));
+        },
+        failure: function (response) {
+            alert(JSON.stringify(response));
+        }
+    });
+}
+
+function autocompleteCategories(request, response) {
+    $.ajax({
+        url: "/api/Autocomplete/GetAutocompleteCategories",
+        data: { text: request.term },
+        type: "GET",
+        success: function (data) {
+            response($.map(data, function (item) {
+                return item;
+            }))
+        },
+        error: function (response) {
+            alert(JSON.stringify(response));
+        },
+        failure: function (response) {
+            alert(JSON.stringify(response));
+        }
+    });
+}
