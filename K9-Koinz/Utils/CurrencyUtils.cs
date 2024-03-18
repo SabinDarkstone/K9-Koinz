9c06c70bb6047b3b06a94e182b33159f1f12d20a
Matthew Camp <sabin.darkstone@gmail.com>
2024-03-18 16:29:58 -0400

    Fixing currency format issue

--- K9-Koinz/Utils/CurrencyUtils.cs
+++ K9-Koinz/Utils/CurrencyUtils.cs
@@ -5,7 +5,8 @@ namespace K9_Koinz.Utils {
         public static string FormatCurrency(this double value, int decimalPlaces = 2) {
             var culture = CultureInfo.CreateSpecificCulture("en-US");
             culture.NumberFormat.CurrencyDecimalDigits = decimalPlaces;
-            culture.NumberFormat.NumberDecimalSeparator = ",";
+            culture.NumberFormat.CurrencyDecimalSeparator = ".";
+            culture.NumberFormat.CurrencyGroupSeparator = ",";
             culture.NumberFormat.CurrencyNegativePattern = 1;
             return string.Format(culture, "{0:C}", value);
         }
