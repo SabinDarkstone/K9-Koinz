e1bf14477b0c370e8e8e97c1da1ca66c60bdf270
Matthew Camp <matthewcampdev@gmail.com>
2024-03-31 18:17:05 -0400

    Fixing bills not selectable on mobile and labels overlapping autocompletes

--- K9-Koinz/wwwroot/js/mobileListInteraction.js
+++ K9-Koinz/wwwroot/js/mobileListInteraction.js
@@ -2,3 +2,8 @@
     window.location = "/Transactions/Details?id=" + $(this).data("id");
     return false;
 });
+
+$(".bill-item").click(function () {
+    window.location = "/Bills/Details?id=" + $(this).data("id");
+    return false;
+});
