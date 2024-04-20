b8ccbc82fbe61de709b5a381151ca7e343906f52
Matthew Camp <matthewcampdev@gmail.com>
2024-04-20 10:41:52 -0400

    Adding background color to amount fields for transactions

---
+++ K9-Koinz/wwwroot/js/inputActions.js
@@ -0,0 +1,13 @@
+﻿function changeAmountColor(field) {
+    let amount = parseFloat(field.val());
+    if (amount > 0) {
+        field.addClass("txt-positive");
+        field.removeClass("txt-negative");
+    } else if (amount < 0) {
+        field.addClass("txt-negative");
+        field.removeClass("txt-positive");
+    } else {
+        field.removeClass("txt-negative");
+        field.removeClass("txt-positive");
+    }
+}
