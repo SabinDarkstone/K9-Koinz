bc97f77459d9e26864805bd3a576c4740cca7647
Matthew Camp <matthewcampdev@gmail.com>
2024-04-21 15:49:31 -0400

    Adding replace with functionality for deleted categories and merchants.  Rollback not working yet

--- K9-Koinz/Pages/Categories/Delete.cshtml.cs
+++ K9-Koinz/Pages/Categories/Delete.cshtml.cs
@@ -2,9 +2,15 @@ using Microsoft.EntityFrameworkCore;
 using K9_Koinz.Data;
 using K9_Koinz.Models;
 using K9_Koinz.Pages.Meta;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace K9_Koinz.Pages.Categories {
     public class DeleteModel : AbstractDeleteModel<Category> {
+        [Display(Name = "Replace Category With")]
+        [BindProperty]
+        public Guid? SelectedCategoryId { get; set; }
+
         public DeleteModel(KoinzContext context, ILogger<AbstractDbPage> logger)
             : base(context, logger) { }
 
@@ -16,6 +22,104 @@ namespace K9_Koinz.Pages.Categories {
 
         protected override void AdditioanlDatabaseActions() {
             _context.Categories.RemoveRange(Record.ChildCategories);
+        }
+
+        protected override void BeforeDeleteActions() {
+            if (SelectedCategoryId.HasValue) {
+                var targetCategory = _context.Categories.Find(SelectedCategoryId.Value);
+
+                var transactionsToUpdate = _context.Transactions
+                    .Where(trans => trans.CategoryId == Record.Id)
+                    .ToList();
+
+                var billsToUpdate = _context.Bills
+                    .Where(bill => bill.CategoryId == Record.Id)
+                    .ToList();
+
+                var transfersToUpdate = _context.Transfers
+                    .Where(fer => fer.CategoryId == Record.Id)
+                    .ToList();
+
+                var categoriesToUpdate = _context.Categories
+                    .Where(cat => cat.ParentCategoryId == Record.Id)
+                    .ToList();
+
+                foreach (var trans in transactionsToUpdate) {
+                    trans.CategoryId = targetCategory.Id;
+                    trans.CategoryName = targetCategory.Name;
+                }
+
+                foreach (var bill in billsToUpdate) {
+                    bill.CategoryId = targetCategory.Id;
+                    bill.CategoryName = targetCategory.Name;
+                }
+
+                foreach (var fer in transfersToUpdate) {
+                    fer.CategoryId = targetCategory.Id;
+                }
+
+                foreach (var cat in categoriesToUpdate) {
+                    cat.ParentCategoryId = targetCategory.Id;
+                    cat.ParentCategoryName = targetCategory.Name;
+                }
+
+                var dbTransaction = _context.Database.BeginTransaction();
+                dbTransaction.CreateSavepoint("BeforeReplacement");
+                try {
+                    _context.UpdateRange(transactionsToUpdate);
+                    _context.UpdateRange(billsToUpdate);
+                    _context.UpdateRange(transfersToUpdate);
+                    _context.UpdateRange(categoriesToUpdate);
+
+                    _context.SaveChanges();
+
+                    dbTransaction.Commit();
+
+                    var transactionIds = transactionsToUpdate.Select(trans => trans.Id).ToList();
+                    var billIds = billsToUpdate.Select(bill => bill.Id).ToList();
+                    var transferIds = transfersToUpdate.Select(fer => fer.Id).ToList();
+                    var categoryIds = categoriesToUpdate.Select(cat => cat.Id).ToList();
+
+                    var updatedTransactions = _context.Transactions
+                        .Where(trans => transactionIds.Contains(trans.Id))
+                        .ToList();
+                    foreach (var trans in updatedTransactions) {
+                        if (trans.CategoryId != targetCategory.Id && trans.CategoryName != targetCategory.Name) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+
+                    var updatedBills = _context.Bills
+                        .Where(bill => billIds.Contains(bill.Id))
+                        .ToList();
+                    foreach (var bill in updatedBills) {
+                        if (bill.CategoryId != targetCategory.Id && bill.MerchantName != targetCategory.Name) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+
+                    var updatedTransfers = _context.Transfers
+                        .Where(fer => transferIds.Contains(fer.Id))
+                        .ToList();
+                    foreach (var fer in updatedTransfers) {
+                        if (fer.CategoryId != targetCategory.Id) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+
+                    var updatedCategories = _context.Categories
+                        .Where(cat => categoryIds.Contains(cat.Id))
+                        .ToList();
+                    foreach (var cat in updatedCategories) {
+                        if (cat.ParentCategoryId != targetCategory.Id) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+                } catch (Exception) {
+                    dbTransaction.RollbackToSavepoint("BeforeReplacement");
+                    throw;
+                }
+            }
         }
     }
 }
--- K9-Koinz/Pages/Merchants/Delete.cshtml.cs
+++ K9-Koinz/Pages/Merchants/Delete.cshtml.cs
@@ -2,9 +2,16 @@ using Microsoft.EntityFrameworkCore;
 using K9_Koinz.Data;
 using K9_Koinz.Models;
 using K9_Koinz.Pages.Meta;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using K9_Koinz.Utils;
 
 namespace K9_Koinz.Pages.Merchants {
     public class DeleteModel : AbstractDeleteModel<Merchant> {
+        [Display(Name = "Replace Merchant With")]
+        [BindProperty]
+        public Guid? SelectedMerchantId { get; set; }
+
         public DeleteModel(KoinzContext context, ILogger<AbstractDbPage> logger)
             : base(context, logger) { }
 
@@ -12,6 +19,86 @@ namespace K9_Koinz.Pages.Merchants {
             return await _context.Merchants
                 .Include(merch => merch.Transactions)
                 .FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        protected override void BeforeDeleteActions() {
+            if (SelectedMerchantId.HasValue) {
+                var targetMerchant = _context.Merchants.Find(SelectedMerchantId.Value);
+
+                var transactionsToUpdate = _context.Transactions
+                    .Where(trans => trans.MerchantId == Record.Id)
+                    .ToList();
+
+                var billsToUpdate = _context.Bills
+                    .Where(bill => bill.MerchantId == Record.Id)
+                    .ToList();
+
+                var transfersToUpdate = _context.Transfers
+                    .Where(fer => fer.MerchantId == Record.Id)
+                    .ToList();
+
+                foreach (var trans in transactionsToUpdate) {
+                    trans.MerchantId = targetMerchant.Id;
+                    trans.MerchantName = targetMerchant.Name;
+                }
+
+                foreach (var bill in billsToUpdate) {
+                    bill.MerchantId = targetMerchant.Id;
+                    bill.MerchantName = targetMerchant.Name;
+                }
+
+                foreach (var fer in transfersToUpdate) {
+                    fer.MerchantId = targetMerchant.Id;
+                }
+
+                var dbTransaction = _context.Database.BeginTransaction();
+                try {
+                    _context.UpdateRange(transactionsToUpdate);
+                    _context.UpdateRange(billsToUpdate);
+                    _context.UpdateRange(transfersToUpdate);
+
+                    _context.SaveChanges();
+
+                    dbTransaction.Commit();
+
+                    var transactionIds = transactionsToUpdate.Select(trans => trans.Id).ToList();
+                    var billIds = billsToUpdate.Select(bill => bill.Id).ToList();
+                    var transferIds = transfersToUpdate.Select(fer => fer.Id).ToList();
+
+                    var updatedTransactions = _context.Transactions
+                        .AsNoTracking()
+                        .Where(trans => transactionIds.Contains(trans.Id))
+                        .ToList();
+                    foreach (var trans in updatedTransactions) {
+                        if (trans.MerchantId != targetMerchant.Id && trans.MerchantName != targetMerchant.Name) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+
+                    var updatedBills = _context.Bills
+                        .AsNoTracking()
+                        .Where(bill => billIds.Contains(bill.Id))
+                        .ToList();
+                    foreach (var bill in updatedBills) {
+                        if (bill.MerchantId != targetMerchant.Id && bill.MerchantName != targetMerchant.Name) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+
+                    var updatedTransfers = _context.Transfers
+                        .AsNoTracking()
+                        .Where(fer => transferIds.Contains(fer.Id))
+                        .ToList();
+                    foreach (var fer in updatedTransfers) {
+                        if (fer.MerchantId != targetMerchant.Id) {
+                            throw new Exception("Replacement failed");
+                        }
+                    }
+                } catch (Exception) {
+                    dbTransaction.Rollback();
+                    RedirectToPage(PagePaths.MerchantIndex);
+                }
+            }
         }
     }
 }
