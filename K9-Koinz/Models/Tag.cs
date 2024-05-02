ba37c55f1e7a81c24ae47b3e293a708698a10db6
Matthew Camp <matthewcampdev@gmail.com>
2024-05-02 15:41:20 -0400

    Upgrading tags pages to match the rest of the site

--- K9-Koinz/Models/Tag.cs
+++ K9-Koinz/Models/Tag.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using K9_Koinz.Models.Meta;
 
 namespace K9_Koinz.Models {
@@ -8,7 +9,10 @@ namespace K9_Koinz.Models {
 
         [Required]
         [StringLength(1)]
+        [DisplayName("Short Form")]
         public string ShortForm { get; set; }
+        [Required]
+        [DisplayName("Badge Color")]
         public string HexColor { get; set; }
 
         public ICollection<Transaction> Transactions { get; set; }
