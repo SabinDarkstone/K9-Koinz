273bd076934d326e3e557a987a47707900526420
Matthew Camp <matthewcampdev@gmail.com>
2024-05-04 14:32:27 -0400

    Cascading delete on recurring transfers

---
+++ K9-Koinz/Migrations/20240504182519_CascadeScheduledTransfers.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace K9_Koinz.Migrations
+{
+    /// <inheritdoc />
+    public partial class CascadeScheduledTransfers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfer_Transfer_RecurringTransferId",
+                table: "Transfer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfer_Transfer_RecurringTransferId",
+                table: "Transfer",
+                column: "RecurringTransferId",
+                principalTable: "Transfer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Transfer_Transfer_RecurringTransferId",
+                table: "Transfer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Transfer_Transfer_RecurringTransferId",
+                table: "Transfer",
+                column: "RecurringTransferId",
+                principalTable: "Transfer",
+                principalColumn: "Id");
+        }
+    }
+}
