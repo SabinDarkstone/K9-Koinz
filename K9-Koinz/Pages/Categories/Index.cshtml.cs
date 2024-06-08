a99dbfd7f1b2f1c1ee3b6c147bb7132b112d60ec
Matthew Camp <matthewcampdev@gmail.com>
2024-06-08 14:49:10 -0400

    Adding retire toggle to categories

--- K9-Koinz/Pages/Categories/Index.cshtml.cs
+++ K9-Koinz/Pages/Categories/Index.cshtml.cs
@@ -2,10 +2,14 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using K9_Koinz.Data;
 using K9_Koinz.Models;
+using System.ComponentModel;
 
 namespace K9_Koinz.Pages.Categories {
     public class IndexModel : PageModel {
         private readonly KoinzContext _context;
+
+        [DisplayName("Show All Categories")]
+        public bool ShowAllCategories { get; set; } = false;
 
         public IndexModel(KoinzContext context) {
             _context = context;
@@ -13,15 +17,25 @@ namespace K9_Koinz.Pages.Categories {
 
         public IList<Category> Categories { get; set; } = default!;
 
-        public async Task OnGetAsync() {
-            var catList = await _context.Categories
+        public async Task OnGetAsync(string viewAll) {
+            if (viewAll == "yes") {
+                ShowAllCategories = true;
+            } else {
+                ShowAllCategories = false;
+            }
+
+            IQueryable<Category> categoriesIQ = _context.Categories
                 .AsNoTracking()
                 .Include(cat => cat.ChildCategories)
                     .ThenInclude(cCat => cCat.Transactions)
                 .Include(cat => cat.Transactions)
                 .OrderBy(cat => cat.CategoryType)
-                    .ThenBy(cat => cat.Name)
-                .ToListAsync();
+                    .ThenBy(cat => cat.Name);
+
+            if (!ShowAllCategories) {
+                categoriesIQ = categoriesIQ.Where(cat => !cat.IsRetired);
+            }
+            var catList = await categoriesIQ.ToListAsync();
 
             Categories = catList
                 .Where(cat => !cat.IsChildCategory)
