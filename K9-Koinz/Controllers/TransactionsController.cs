c7528693a7dfdd45dba8f13b3d20b3143dcb3688
Matthew Camp <sabin.darkstone@gmail.com>
2025-02-05 11:05:38 -0500

    Adding api for shortcuts

--- K9-Koinz/Controllers/TransactionsController.cs
+++ K9-Koinz/Controllers/TransactionsController.cs
@@ -1,4 +1,5 @@
 ﻿using K9_Koinz.Data;
+using K9_Koinz.Models;
 using K9_Koinz.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,93 @@ namespace K9_Koinz.Controllers {
 
         public TransactionsController(KoinzContext context) {
             _context = context;
+        }
+
+        private struct CreateTransactionResponse {
+            public bool isSuccess { get; set; }
+            public string errorMessage { get; set; }
+            public Guid transactionId { get; set; }
+        }
+
+        public class CreateTransactionDTO {
+            public string accountName { get; set; }
+            public string merchantName { get; set; }
+            public string categoryName { get; set; }
+            public double amount { get; set; }
+        }
+        
+
+        [HttpPost]
+        public async Task<JsonResult> CreateTransaction([FromBody] CreateTransactionDTO dto) {
+            var account = await _context.Accounts
+                .AsNoTracking()
+                .FirstOrDefaultAsync(acct => acct.Name == dto.accountName);
+            var merchant = await _context.Merchants
+                .AsNoTracking()
+                .FirstOrDefaultAsync(merch => merch.Name == dto.merchantName);
+                var category = await _context.Categories
+                .AsNoTracking()
+                .FirstOrDefaultAsync (cat => cat.Name == dto.categoryName);
+
+            if (account == null) {
+                var errResponse = new CreateTransactionResponse {
+                    isSuccess = false,
+                    errorMessage = "Unknown account: " + dto.accountName,
+                    transactionId = Guid.Empty
+                };
+                return Json(errResponse);
+            } else if (category == null) {
+                var errResponse = new CreateTransactionResponse {
+                    isSuccess = false,
+                    errorMessage = "Unknown category: " + dto.categoryName,
+                    transactionId = Guid.Empty
+                };
+                return Json(errResponse);
+            } else if (merchant == null) {
+                var errResponse = new CreateTransactionResponse {
+                    isSuccess = false,
+                    errorMessage = "Unknown merchant: " + dto.merchantName,
+                    transactionId = Guid.Empty
+                };
+                return Json(errResponse);
+            }
+
+            var newTransaction = new Transaction {
+                AccountId = account.Id,
+                AccountName = dto.accountName,
+                MerchantId = merchant.Id,
+                MerchantName = dto.merchantName,
+                CategoryId = category.Id,
+                CategoryName = dto.categoryName,
+                Amount = dto.amount,
+                Date = DateTime.Now,
+                TransferId = null,
+                BillId = null,
+                SavingsGoalId = null,
+                IsSavingsSpending = false,
+                TagId = null,
+                ParentTransactionId = null,
+                Notes = "Created from API"
+            };
+
+            try {
+                _context.Transactions.Add(newTransaction);
+                await _context.SaveChangesAsync();
+            } catch (Exception ex) {
+                var errResponse = new CreateTransactionResponse {
+                    errorMessage = ex.Message,
+                    isSuccess = false,
+                    transactionId = Guid.Empty
+                };
+                return Json(errResponse);
+            }
+
+            var response = new CreateTransactionResponse {
+                errorMessage = null,
+                isSuccess = true,
+                transactionId = newTransaction.Id
+            };
+            return Json(response);
         }
 
         public async Task<JsonResult> GetTransactionsFromTrendAsync(string categoryId, string month, int year) {
